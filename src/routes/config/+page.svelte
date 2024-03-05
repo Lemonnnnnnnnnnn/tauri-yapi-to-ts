798fddf9cf832d52950ade87d08c4dab25172477
soul <104170577@qq.com>
2024-03-05 20:53:32 +0800

    feat: config page

--- src/routes/config/+page.svelte
+++ src/routes/config/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import { toastTheme } from '@/consts';
 	import type { Config, GlobalConfig, SuccessResponse } from '@/types/public';
-	import { request } from '@/utils';
 	import { toast } from '@zerodevx/svelte-toast';
 	import Tooltip, { Wrapper } from '@smui/tooltip';
 	import Textfield from '@smui/textfield';
-	import Button from '@smui/button';
 
 	import { onMount } from 'svelte';
 	import { sourcePath } from '@/store';
@@ -32,7 +30,10 @@
 
 	onMount(() => {
 		invoke<SuccessResponse<GlobalConfig>>('load_global_config', {}).then((res) => {
+			console.log(res);
+
 			globalConfig = res.data;
+			console.log(globalConfig);
 		});
 
 		invoke<SuccessResponse<Config>>('load_project_config', { sourcePath: $sourcePath }).then(
@@ -46,25 +47,17 @@
 		invoke<SuccessResponse<String>>('update_project_config', {
 			sourcePath: $sourcePath,
 			data: projectConfig
-		})
-			.then((res) => {
-				toast.push(res.message, toastTheme.success);
-			})
-			.catch((e) => {
-				toast.push(JSON.stringify(e), toastTheme.error);
-			});
+		}).catch((e) => {
+			toast.push(JSON.stringify(e), toastTheme.error);
+		});
 	}
 
 	function update_global_config() {
 		invoke<SuccessResponse<String>>('update_global_config', {
 			data: globalConfig
-		})
-			.then((res) => {
-				toast.push(res.message, toastTheme.success);
-			})
-			.catch((e) => {
-				toast.push(JSON.stringify(e), toastTheme.error);
-			});
+		}).catch((e) => {
+			toast.push(JSON.stringify(e), toastTheme.error);
+		});
 	}
 </script>
 
@@ -77,6 +70,9 @@
 			variant="outlined"
 			bind:value={projectConfig.base_url}
 			label="Yapi地址根路径"
+			on:blur={() => {
+				update_project_config();
+			}}
 		></Textfield>
 	</div>
 	<div>
@@ -95,8 +91,11 @@
 			style="width:100%"
 			type="number"
 			variant="outlined"
-			bind:value={initGlobalConfig.rate_limit}
+			bind:value={globalConfig.rate_limit}
 			label="请求yapi-openapi最大并行请求数"
+			on:blur={() => {
+				update_global_config();
+			}}
 		></Textfield>
 	</div>
 	<div>
@@ -104,8 +103,11 @@
 			style="width:100%"
 			type="number"
 			variant="outlined"
-			bind:value={initGlobalConfig.break_seconds}
+			bind:value={globalConfig.break_seconds}
 			label="请求yapi-openapi时间间隔"
+			on:blur={() => {
+				update_global_config();
+			}}
 		></Textfield>
 	</div>
 	<div>
@@ -114,6 +116,9 @@
 			variant="outlined"
 			bind:value={projectConfig.request_path}
 			label="你想要把定义 request 的 ts 文件放到哪个文件夹下？"
+			on:blur={() => {
+				update_project_config();
+			}}
 		></Textfield>
 	</div>
 
@@ -124,6 +129,9 @@
 				variant="outlined"
 				bind:value={projectConfig.request_template}
 				label="请求模板字符串"
+				on:blur={() => {
+					update_project_config();
+				}}
 			></Textfield>
 			<Tooltip>
 				<p>$1: 请求名</p>
@@ -140,6 +148,9 @@
 				variant="outlined"
 				bind:value={projectConfig.type_import_template}
 				label="import类型模板"
+				on:blur={() => {
+					update_project_config();
+				}}
 			></Textfield>
 			<Tooltip>
 				<p>$1: Request Type 类型</p>
@@ -155,6 +166,9 @@
 			variant="outlined"
 			bind:value={projectConfig.header_template}
 			label="请求文件首部字符串"
+			on:blur={() => {
+				update_project_config();
+			}}
 		></Textfield>
 	</div>
 
@@ -165,6 +179,9 @@
 				variant="outlined"
 				bind:value={projectConfig.file_name_template}
 				label="文件名模板字符串"
+				on:blur={() => {
+					update_project_config();
+				}}
 			></Textfield>
 
 			<Tooltip>
