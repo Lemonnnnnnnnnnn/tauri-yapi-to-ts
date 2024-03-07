f06718a717675f0450a2d4c4dcd620888b93e36f
soul <104170577@qq.com>
2024-03-07 23:06:43 +0800

    refactor: simgle interface add to task

--- src/routes/type/components/ProjectTab/Interface.svelte
+++ src/routes/type/components/ProjectTab/Interface.svelte
@@ -1,41 +1,22 @@
 <script lang="ts">
-	import { toast } from '@zerodevx/svelte-toast';
-	import type { InterfaceType, SuccessResponse } from '@/types/public';
-	import { request } from '@/utils';
-	import { toastTheme } from '@/consts';
-	import { processingModalTotal, processingModalOpen, runningTask } from '@/store';
-	import { confirm } from '@tauri-apps/api/dialog';
+	import type { InterfaceType } from '@/types/public';
+	import { startTask } from '@/utils';
+	import { sourcePath } from '@/store';
+	import Tooltip, { Wrapper } from '@smui/tooltip';
+	import { invoke } from '@tauri-apps/api';
 
 	export let data: InterfaceType;
 	export let token: string;
 
 	async function update_interface(id: string) {
-		if ($runningTask) {
-			toast.push('正在执行任务...请稍等', toastTheme.error);
-			return;
-		}
-		const confirmed = await confirm('操作将重新生成ts文件，请确保本地代码已经保存！');
-
-		if (!confirmed) return;
-
-		let interfaces = [
-			{
-				id
+		await invoke('add_interface_task', {
+			data: {
+				token,
+				source_path: $sourcePath,
+				interface_id: id
 			}
-		];
-
-		toast.push('正在添加任务...');
-		request('update_interface', { interfaces, token })
-			// @ts-expect-error
-			.then((res: SuccessResponse<number>) => {
-				toast.push(res.message, toastTheme.success);
-				processingModalOpen.update(() => true);
-				processingModalTotal.update(() => res.data);
-				runningTask.update(() => true);
-			})
-			.catch((e) => {
-				toast.push(JSON.stringify(e), toastTheme.error);
-			});
+		});
+		startTask();
 	}
 </script>
 
@@ -51,7 +32,10 @@
 			role="button"
 			tabindex="0"
 		>
-			<img class="icon" src="/full_update.svg" alt="更新接口" />
+			<Wrapper>
+				<img class="icon" src="/full_update.svg" alt="更新接口" />
+				<Tooltip>更新接口</Tooltip>
+			</Wrapper>
 		</div>
 	</div>
 </div>
